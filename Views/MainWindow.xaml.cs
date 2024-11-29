f188d8a43faff2cdfef3809d39ed0176b7aa4d0c
alex.js <zhongwcool@163.com>
2024-11-29 16:19:17 +0800

    主题变化和渐变

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using System.Windows;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
 using UsbMonitor.Helpers;
 using UsbMonitor.ViewModels;
 
@@ -11,6 +13,20 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = new MainViewModel();
+
+        // 加载主题
+        UpdateTheme();
+        // 监听系统主题变化
+        SystemEvents.UserPreferenceChanged += (_, args) =>
+        {
+            // 当事件是由于主题变化引起的
+            if (args.Category == UserPreferenceCategory.General)
+            {
+                // 这里你可以写代码来处理主题变化，例如，重新加载样式或者资源
+                UpdateTheme();
+            }
+        };
+
         _comHelper = new ComHelper(this);
 
         var version = Assembly.GetExecutingAssembly().GetName().Version;
@@ -36,4 +52,26 @@ public partial class MainWindow : Window
 
         if (DataContext is MainViewModel vm) vm.Dispose();
     }
+
+    #region Theme Change
+
+    private static void UpdateTheme()
+    {
+        var paletteHelper = new PaletteHelper();
+        var theme = paletteHelper.GetTheme();
+        // 检查当前主题并应用
+        switch (Theme.GetSystemTheme())
+        {
+            case BaseTheme.Light:
+                theme.SetBaseTheme(BaseTheme.Light);
+                break;
+            case BaseTheme.Dark:
+                theme.SetBaseTheme(BaseTheme.Dark);
+                break;
+        }
+
+        paletteHelper.SetTheme(theme);
+    }
+
+    #endregion
 }
