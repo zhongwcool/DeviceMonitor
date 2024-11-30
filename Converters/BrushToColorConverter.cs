fb2580b7d05940924bb95fa3cc8925bdfd316a8d
alex.js <zhongwcool@163.com>
2024-11-30 13:02:33 +0800

    优化操作日志的遮罩效果

---
+++ Converters/BrushToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace UsbMonitor.Converters;
+
+public class BrushToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not SolidColorBrush brush)
+            return Colors.Transparent;
+
+        if (parameter == null || !double.TryParse(parameter.ToString(), NumberStyles.Float,
+                CultureInfo.InvariantCulture, out var opacity)) return brush.Color;
+        var color = brush.Color;
+        var newAlpha = (byte)(color.A * opacity); // Calculate new alpha value
+
+        // Return new color with adjusted alpha
+        return Color.FromArgb(newAlpha, color.R, color.G, color.B);
+
+        // If opacity parameter is not valid, return the original color
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
